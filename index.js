cf134e265bdce8730c0e5ec3f8c275dc86ea5a27
Hugo Giraudel <hugo.giraudel@gmail.com>
2014-07-04 22:58:32 +0200

    Improved code quality

--- index.js
+++ index.js
@@ -3,39 +3,42 @@ var CDocParser = require('cdocparser');
 /**
  * SCSS Context Parser
  */
-var scssContextParser = (function(){
+var scssContextParser = (function () {
   var ctxRegEx = /(@|\$)(\w+)(?:\s+(\w+)){0,1}/m;
-  var parser = function( ctxCode ) {
+  var parser = function (ctxCode) {
     var match = ctxRegEx.exec(ctxCode);
     var context = {};
-    if ( match ) {
-      switch (match[1]){
-        case "@" : // Mixin/fucntion
-          context.type = match[2]; // mixin/function
+    
+    if (match) {
+      switch (match[1]) {
+        case "@": // Mixin/function
+          context.type = match[2];
           context.name = match[3];
           break;
-        case "$" :
+        case "$":
           context.type = 'variable';
           context.name = match[2];
           break;
-        default :
+        default:
           context.type = 'unknown';
       }
     }
+    
     return context;
   };
+  
   return parser;
 })();
 
 
-var extractor = new CDocParser.CommentExtractor( scssContextParser );
+var extractor = new CDocParser.CommentExtractor(scssContextParser);
 
-var Parser = function( annotations ){
-  this.commentParser = new CDocParser.CommentParser( annotations );
+var Parser = function (annotations) {
+  this.commentParser = new CDocParser.CommentParser(annotations);
 };
 
-Parser.prototype.parse = function( code ){
-  var comments = extractor.extract ( code );
+Parser.prototype.parse = function (code) {
+  var comments = extractor.extract(code);
   return this.commentParser.parse(comments);
 };
 
